c988fd9909030b8496489ea908893f953028c1ed
Dave Ariens <dave@ariens.ca>
2014-11-05 00:39:18 -0500

    being lazy

--- app/emails.py
+++ app/emails.py
@@ -14,7 +14,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 def send_user_email_activation(email_activation):
     user = User.query.filter_by(id=email_activation.user_id).first()
     email_address = UserEmailAddress.query.filter_by(id=email_activation.email_address_id).first()
-    send_email("[www.fourtitude.ca] Activate Your Account",
+    send_email("[www.ariens.ca] Activate Your Account",
                "dave@ariens.ca",
                [email_address.email_address],
                render_template("auth/emails/email_activate_account.txt", user=user, activation=email_activation),
@@ -23,7 +23,7 @@ def send_user_email_activation(email_activation):
 def send_reset_password_activation(email_activation):
     user = User.query.filter_by(id=email_activation.user_id).first()
     email_address = UserEmailAddress.query.filter_by(id=email_activation.email_address_id).first()
-    send_email("[www.fourtitude.ca] Reset Your Password",
+    send_email("[www.ariens.ca] Reset Your Password",
                "dave@ariens.ca",
                [email_address.email_address],
                render_template("auth/emails/email_password_reset.txt", user=user, activation=email_activation),
